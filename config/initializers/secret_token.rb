b2a88843da001e36396d3ad852b5475e4d683bb6
Guilherme <guilhermefeitosa66@gmail.com>
2014-10-15 15:05:11 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sinfo2014::Application.config.secret_token = '1b0b4f2fbad0c2f664d1e1b86b3ca5bbf5fedd26667b34c730952a078629f4446d825b132713dcdf79c20ebbb463f57086bbdc3c18502b505200937b0f1e5078'
